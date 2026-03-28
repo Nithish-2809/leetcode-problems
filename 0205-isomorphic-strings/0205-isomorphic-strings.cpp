33812e732637f7fc0c11a07e6b3fb5be92e109a9
Nithish-2809 <nithishreddy2809@gmail.com>
2026-03-28 23:50:07 +0530

    Time: 7 ms (6.45%), Space: 9.3 MB (80.44%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        int n =  s.length();
+
+        unordered_map<char,char>mapst;
+        unordered_map<char,char>mapts;
+
+        for(int i=0;i<n;i++) {
+            int sChar = s[i];
+            int tChar = t[i];
+
+            if(mapst.find(sChar)!=mapst.end()) {
+                if(mapst[sChar]!=tChar) {
+                    return false;
+                }
+            }
+            else {
+                mapst[sChar] = tChar;
+            }
+
+            if(mapts.find(tChar)!=mapts.end()) {
+                if(mapts[tChar]!=sChar) {
+                    return false;
+                }
+            }
+            else {
+                mapts[tChar] = sChar;
+            }
+        }
+
+    return true;
+    }
+};
