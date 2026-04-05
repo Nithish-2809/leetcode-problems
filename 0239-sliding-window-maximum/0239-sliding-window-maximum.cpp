ecc8a2a0930ea9df434317e408279dee60d5a841
Nithish-2809 <nithishreddy2809@gmail.com>
2026-04-05 13:13:41 +0530

    Time: 27 ms (44.58%), Space: 139.3 MB (26.01%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,23 +5,26 @@ public:
         deque<int> dq;
         vector<int> ans;
 
-        for(int i=0;i<n;i++) {
+        for(int i = 0; i < n; i++) {
 
-            if(!dq.empty() && dq.front()==i-k) {
+            
+            if(!dq.empty() && dq.front() <= i - k) {
                 dq.pop_front();
             }
 
-            while(!dq.empty() && nums[dq.back()]<nums[i]) {
+            
+            while(!dq.empty() && nums[dq.back()] < nums[i]) {
                 dq.pop_back();
             }
 
             dq.push_back(i);
 
-            if(i>=k-1) {
+            
+            if(i >= k - 1) {
                 ans.push_back(nums[dq.front()]);
             }
         }
-        
+
         return ans;
     }
 };
