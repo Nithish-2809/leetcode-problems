bc05554bc0a26a00fa376c7fc175d73fb3c0c78c
Nithish-2809 <nithishreddy2809@gmail.com>
2026-07-24 00:34:35 +0530

    Time: 30 ms (22.59%), Space: 50.5 MB (78.76%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -0,0 +1,80 @@
+class Node {
+public:
+    Node* links[26];
+    bool flag;
+
+    Node() {
+        for (int i = 0; i < 26; i++)
+            links[i] = nullptr;
+        flag = false;
+    }
+
+    bool containsKey(char ch) {
+        return links[ch - 'a'] != nullptr;
+    }
+
+    Node* get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    void put(char ch, Node* node) {
+        links[ch - 'a'] = node;
+    }
+
+    void setEnd() {
+        flag = true;
+    }
+
+    bool isEnd() {
+        return flag;
+    }
+};
+
+class Trie {
+private:
+    Node* root;
+
+public:
+    Trie() {
+        root = new Node();
+    }
+
+    void insert(string word) {
+        Node* node = root;
+
+        for (char ch : word) {
+            if (!node->containsKey(ch)) {
+                node->put(ch, new Node());
+            }
+            node = node->get(ch);
+        }
+
+        node->setEnd();
+    }
+
+    bool search(string word) {
+        Node* node = root;
+
+        for (char ch : word) {
+            if (!node->containsKey(ch))
+                return false;
+
+            node = node->get(ch);
+        }
+
+        return node->isEnd();
+    }
+
+    bool startsWith(string prefix) {
+        Node* node = root;
+
+        for (char ch : prefix) {
+            if (!node->containsKey(ch))
+                return false;
+
+            node = node->get(ch);
+        }
+
+        return true;
+    }
+};
