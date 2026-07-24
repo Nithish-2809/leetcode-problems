002d5a6f0a4094acd5076f1db85b1e68c8724a7b
Nithish-2809 <nithishreddy2809@gmail.com>
2026-07-25 00:18:13 +0530

    Time: 0 ms (100%), Space: 8.6 MB (20.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,14 @@
-int countWays(int n,vector<int>&dp) {
-    if(n<=1) return 1;
-
-    if(dp[n]!=-1) return dp[n];
-
-    return dp[n] = countWays(n-1,dp)+countWays(n-2,dp);
-}
-
 class Solution {
 public:
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        
+        dp[0] = 1;
+        dp[1] = 1;
 
-        return countWays(n,dp);
+        for(int i=2;i<=n;i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+    return dp[n];
     }
 };
