cba7ce2bf607e945e8e0d8692c5b89ed34367f26
Nithish-2809 <nithishreddy2809@gmail.com>
2026-07-25 23:06:04 +0530

    Time: 0 ms (100%), Space: 10.6 MB (64.58%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int index,vector<int>&dp) {
+        int n = nums.size();
+
+        if (index >= n)
+            return 0;
+        
+        if(dp[index]!=-1) return dp[index];
+
+        int pick = nums[index] + solve(nums, index + 2,dp);
+        int notPick = solve(nums, index + 1,dp);
+
+        return dp[index] =  max(pick, notPick);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int>dp(n,-1);
+        return solve(nums, 0,dp);
+    }
+};
