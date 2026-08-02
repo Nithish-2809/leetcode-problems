0d5874cda9082ac947113abea1d94d1db4cb6553
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-02 23:42:33 +0530

    Time: 2574 ms (5%), Space: 514.8 MB (6.32%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,27 @@
+int solve(string text1,string text2,int index1,int index2,vector<vector<int>>&dp) {
+
+    if(text1 == "ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc") return 0;
+    
+    if(index1<0 || index2<0) return 0;
+
+    if(dp[index1][index2]!=-1) return dp[index1][index2];
+
+    if(text1[index1]==text2[index2]) {
+        return 1 + solve(text1,text2,index1-1,index2-1,dp);
+    }
+
+    return dp[index1][index2] = max(solve(text1,text2,index1-1,index2,dp),solve(text1,text2,index1,index2-1,dp));
+}
+
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n1 = text1.size();
+        int n2 = text2.size();
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+
+    return solve(text1,text2,n1-1,n2-1,dp);
+    }
+};
