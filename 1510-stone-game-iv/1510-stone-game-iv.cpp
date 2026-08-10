2c25d8258096c483aae9709a2e2e70217813a888
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-10 12:53:10 +0530

    Time: 42 ms (61.41%), Space: 16.8 MB (39.1%) - LeetHub

---
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -0,0 +1,21 @@
+bool solve(int n,vector<int>&dp) {
+    if(n==0) return false;
+
+    if(dp[n]!=-1) return dp[n];
+
+    for(int i=1;i*i<=n;i++) {
+        if(!solve(n-(i*i),dp)) return dp[n] = true;
+    }
+
+    return dp[n] = false;
+}
+
+
+
+class Solution {
+public:
+    bool winnerSquareGame(int n) {
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
