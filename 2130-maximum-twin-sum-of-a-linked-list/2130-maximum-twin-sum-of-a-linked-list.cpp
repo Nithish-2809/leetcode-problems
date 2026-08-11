3634c21480249ed09fcbfd4cd54367aec1286d49
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-11 19:02:37 +0530

    Time: 3 ms (66.18%), Space: 124.4 MB (67.71%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,34 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ListNode* reverseSinglyLinkedList(ListNode *head) {
+    ListNode *prev = NULL;
+    ListNode *curr = head;
+
+    while(curr!=NULL) {
+        ListNode *nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+
+    return prev;
+}
+
+
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
+        ListNode *temp = head;
+        int maxi = INT_MIN;
 
-        while (fast && fast->next) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+
+        while(fast!=NULL) {
             slow = slow->next;
             fast = fast->next->next;
         }
+        ListNode *secondHead = reverseSinglyLinkedList(slow);
 
-    
-        ListNode* prev = nullptr;
-        while (slow) {
-            ListNode* nextNode = slow->next;
-            slow->next = prev;
-            prev = slow;
-            slow = nextNode;
+        while(secondHead!=NULL) {
+            int sum = temp->val + secondHead->val;
+            maxi = max(maxi,sum);
+            temp = temp->next;
+            secondHead = secondHead->next;
         }
-
-        
-        int maxi = 0;
-        ListNode* first = head;
-        ListNode* second = prev;
-
-        while (second) {
-            maxi = max(maxi, first->val + second->val);
-            first = first->next;
-            second = second->next;
-        }
-
-        return maxi;
+    return maxi;
     }
 };
