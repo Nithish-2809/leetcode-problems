ebbe98ddb6f340c2fa00a30589c814a7b27a2ac4
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-11 20:41:47 +0530

    Time: 0 ms (100%), Space: 19.8 MB (45.43%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,56 +1,37 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right)
- *         : val(x), left(left), right(right) {}
- * };
- */
-
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL) return 0;
+        if (!root) return 0;
 
         long long ans = 0;
 
         queue<pair<TreeNode*, long long>> q;
-
         q.push({root, 0});
 
-        while(!q.empty()) {
-
+        while (!q.empty()) {
             int size = q.size();
 
-            long long mini = q.front().second;
+            long long first = q.front().second;
+            long long last = 0;
 
-            long long first, last;
-
-            for(int i = 0; i < size; i++) {
-
-                TreeNode* node = q.front().first;
-
-                long long index = q.front().second - mini;
-
+            for (int i = 0; i < size; i++) {
+                auto [node, idx] = q.front();
                 q.pop();
 
-                if(i == 0) first = index;
-                if(i == size - 1) last = index;
+                // Normalize index
+                idx -= first;
 
-                if(node->left) {
-                    q.push({node->left, 2 * index + 1});
-                }
+                if (i == size - 1)
+                    last = idx;
 
-                if(node->right) {
-                    q.push({node->right, 2 * index + 2});
-                }
+                if (node->left)
+                    q.push({node->left, 2 * idx + 1});
+
+                if (node->right)
+                    q.push({node->right, 2 * idx + 2});
             }
 
-            ans = max(ans, last - first + 1);
+            ans = max(ans, last + 1);
         }
 
         return ans;
