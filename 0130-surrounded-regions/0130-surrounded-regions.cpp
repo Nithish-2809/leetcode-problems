d5a8f42607665e254a845416c0dadf514972ed51
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-14 16:25:15 +0530

    Time: 0 ms (100%), Space: 15 MB (28%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -4,16 +4,17 @@ public:
         int n = board.size();
         int m = board[0].size();
         vector<vector<int>>visited(n,vector<int>(m,0));
+
         queue<pair<int,int>>q;
 
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-               if(i==0 || j==0 || i==n-1 || j==m-1) {
-                if(board[i][j]=='O') {
-                    q.push({i,j});
-                    visited[i][j] = 1;
+                if(i==0 || j==0 || i==n-1 || j==m-1) {
+                    if(board[i][j]=='O') {
+                        q.push({i,j});
+                        visited[i][j] =1;
+                    }
                 }
-               }
             }
         }
 
@@ -21,61 +22,32 @@ public:
             int row = q.front().first;
             int col = q.front().second;
             q.pop();
-            
+
             //down
-            if(row<n-1 && !visited[row+1][col]) {
-                if( board[row+1][col]=='O') {
-                    visited[row+1][col] = 1;
-                    q.push({row+1,col});
-                }
-                else {
-                    visited[row+1][col] = 2;
-                }
+            if(row+1<n && board[row+1][col]=='O' && !visited[row+1][col]) {
+                visited[row+1][col] = 1;
+                q.push({row+1,col});
             }
-
             //up
-            if(row>0 && !visited[row-1][col]) {
-                if( board[row-1][col]=='O') {
-                    visited[row-1][col] = 1;
-                    q.push({row-1,col});
-                }
-                else {
-                    visited[row-1][col] = 2;
-                }
+            if(row-1>=0 && board[row-1][col]=='O' && !visited[row-1][col]) {
+                visited[row-1][col] = 1;
+                q.push({row-1,col});
             }
-
             //right
-
-            if(col<m-1 && !visited[row][col+1]) {
-                if( board[row][col+1]=='O') {
-                    visited[row][col+1] = 1;
-                    q.push({row,col+1});
-                }
-                else {
-                    visited[row][col+1] = 2;
-                }
+            if(col+1<m && board[row][col+1]=='O' && !visited[row][col+1]) {
+                visited[row][col+1] = 1;
+                q.push({row,col+1});
             }
-
             //left
-            if(col>0 && !visited[row][col-1]) {
-                if( board[row][col-1]=='O') {
-                    visited[row][col-1] = 1;
-                    q.push({row,col-1});
-                }
-                else {
-                    visited[row][col-1] = 2;
-                }
+            if(col-1>=0 && board[row][col-1]=='O' && !visited[row][col-1]) {
+                visited[row][col-1] = 1;
+                q.push({row,col-1});
             }
         }
 
         for(int i=0;i<n;i++) {
             for(int j=0;j<m;j++) {
-                if(visited[i][j]==1) {
-                    board[i][j] = 'O';
-                }
-                else {
-                    board[i][j] = 'X';
-                }
+                if(!visited[i][j] && board[i][j]=='O') board[i][j] = 'X';
             }
         }
     }
