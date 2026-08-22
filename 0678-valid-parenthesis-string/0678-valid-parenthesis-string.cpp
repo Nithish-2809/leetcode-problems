c0e8cbf6bf3ee70c540f682882b5e481417f0209
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-22 15:16:21 +0530

    Time: 258 ms (4.69%), Space: 246.3 MB (5.15%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,30 +1,39 @@
+bool isValidString(string s,int index,int left,int right,vector<vector<vector<int>>>&dp) {
+    int n = s.length();
+
+    if(right > left) return false;
+
+    if(index == n) {
+        return left == right;
+    }
+
+    if(dp[left][right][index]!=-1) return dp[left][right][index];
+
+    if(s[index] == '(') {
+        return dp[left][right][index] = isValidString(s,index+1,left+1,right,dp);
+    }
+    else if(s[index] == ')') {
+        return dp[left][right][index] = isValidString(s,index+1,left,right+1,dp);
+    }
+
+    return dp[left][right][index] = isValidString(s,index+1,left+1,right,dp) ||
+           isValidString(s,index+1,left,right+1,dp) ||
+           isValidString(s,index+1,left,right,dp);
+}
+
+
+
+
+
 class Solution {
 public:
     bool checkValidString(string s) {
-        int mini = 0;
-        int maxi = 0;
-
         int n = s.length();
+        vector<vector<vector<int>>> dp(
+    n + 1,
+    vector<vector<int>>(n + 1, vector<int>(n + 1, -1))
+);
 
-        for(int i=0;i<n;i++) {
-            if(s[i]=='(') {
-                mini++;
-                maxi++;
-            }
-            else if(s[i]==')') {
-                mini--;
-                maxi--;
-            }
-            else {
-                mini--;
-                maxi++;
-            }
-
-            if(mini<0) mini = 0;
-            if(maxi<0) return false;
-        }
-
-
-        return mini==0;
+        return isValidString(s,0,0,0,dp);
     }
 };
