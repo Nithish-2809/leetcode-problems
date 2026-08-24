93ff9ebf133c86356f9205e141cc9d938704d4fc
Nithish-2809 <nithishreddy2809@gmail.com>
2026-08-25 00:30:26 +0530

    Time: 2 ms (18.75%), Space: 8.5 MB (97.6%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,36 +1,30 @@
-void generateSubsequences(int k,int n,int sum,vector<vector<int>>&ans,vector<int>&ds,int index) {
-    
-    if(sum > n) return;
-        if(ds.size() == k) {
+void solve(int k, int n, int currSum, int start,
+           vector<vector<int>>& ans, vector<int>& curr) {
 
-            if(sum == n) {
-                ans.push_back(ds);
-            }
-
-            return;
-        }
-        if(index > 9) return;
-
-        
-        sum += index;
-        ds.push_back(index);
-
-        generateSubsequences(k,n,sum,ans,ds,index + 1);
-        sum -= index;
-        ds.pop_back();
-        
-        generateSubsequences(k,n,sum,ans, ds,index + 1);
+    if (curr.size() == k) {
+        if (currSum == n)
+            ans.push_back(curr);
+        return;
     }
 
+    for (int i = start; i <= 9; i++) {
 
+
+        curr.push_back(i);
+
+        solve(k, n, currSum + i, i + 1, ans, curr);
+
+        curr.pop_back();
+    }
+}
 
 class Solution {
 public:
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>>ans;
-        vector<int>ds;
-        int sum = 0;
-        generateSubsequences(k,n,sum,ans,ds,1);
+        vector<vector<int>> ans;
+        vector<int> curr;
+
+        solve(k, n, 0, 1, ans, curr);
 
         return ans;
     }
