8a54c568e0dbfc45c7c6fc55384e7350821d3a32
Nithish-2809 <nithishreddy2809@gmail.com>
2026-09-20 02:23:09 +0530

    Time: 15 ms (60.12%), Space: 46.7 MB (42.78%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,74 +1,66 @@
 class DisjointSet {
 public:
-    vector<int> rank;
     vector<int> parent;
-
+    vector<int> rank;
     DisjointSet(int n) {
         rank.resize(n + 1, 0);
-        parent.resize(n + 1);
+        parent.resize(n + 1, 0);
 
-        for (int i = 0; i <= n; i++) {
+        for(int i = 0; i <= n; i++) {
             parent[i] = i;
         }
     }
 
-    int findUltimateParent(int node) {
-        if (parent[node] == node) {
-            return node;
-        }
-        return parent[node] = findUltimateParent(parent[node]);
+    int findParent(int u) {
+        if(parent[u] == u) return u;
+
+        return parent[u] = findParent(parent[u]);
     }
 
     void unionByRank(int u, int v) {
-        int ultp_u = findUltimateParent(u);
-        int ultp_v = findUltimateParent(v);
+        int ultimateParentU = findParent(u);
+        int ultimateParentV = findParent(v);
 
-        if (ultp_u == ultp_v) {
-            return;
+        if(ultimateParentU == ultimateParentV) return;
+
+        if(rank[ultimateParentU] > rank[ultimateParentV]) {
+            parent[ultimateParentV] = ultimateParentU;
         }
-
-        if (rank[ultp_u] < rank[ultp_v]) {
-            parent[ultp_u] = ultp_v;
-        } else if (rank[ultp_v] < rank[ultp_u]) {
-            parent[ultp_v] = ultp_u;
-        } else {
-            parent[ultp_v] = ultp_u;
-            rank[ultp_u]++;
+        else if(rank[ultimateParentU] < rank[ultimateParentV]) {
+            parent[ultimateParentU] = ultimateParentV;
+        }
+        else {
+            parent[ultimateParentU] = ultimateParentV;
+            rank[ultimateParentV]++;
         }
     }
 };
 
+
 class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-
         DisjointSet ds(n);
-        int extraConnections = 0;
+        int extras = 0;
 
-        for (auto it : connections) {
+        for(auto it : connections) {
             int u = it[0];
             int v = it[1];
 
-            if (ds.findUltimateParent(u) == ds.findUltimateParent(v)) {
-                extraConnections++;
-            } else {
-                ds.unionByRank(u, v);
-            }
+            if(ds.findParent(u)==ds.findParent(v)) extras++;
+            else ds.unionByRank(u,v);
         }
 
-        int numberOfComponents = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (ds.findUltimateParent(i) == i) {
-                numberOfComponents++;
-            }
+        int nC = 0;
+        for(int i=0;i<n;i++) {
+            if(ds.parent[i]==i) nC++;
         }
 
-        int ans = numberOfComponents - 1;
+        int ans = nC-1;
 
-        if (extraConnections >= ans)
+        if(extras>=nC-1) {
             return ans;
-
+        }
         return -1;
     }
 };
