9f1a93fa18db7c79a6e61542d88c39f77db1c344
Nithish-2809 <nithishreddy2809@gmail.com>
2026-09-26 14:21:58 +0530

    Time: 52 ms (33.99%), Space: 10.2 MB (39.51%) - LeetHub

--- 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
+++ 3751-total-waviness-of-numbers-in-range-i/3751-total-waviness-of-numbers-in-range-i.cpp
@@ -1,32 +1,28 @@
-int calculateWaviness(int num) {
-    string s = to_string(num);
+int calculateWaviness(int n) {
 
-    int n = s.size();
-
+    string num = to_string(n);
     int waviness = 0;
 
-    for(int i=1;i<n-1;i++) {
-        if(s[i]-'0'>s[i-1]-'0' && s[i]-'0'>s[i+1]-'0') {
-            waviness++;
-        }
-        else if(s[i]-'0'<s[i-1]-'0' && s[i]-'0'<s[i+1]-'0') {
-            waviness++;
-        }
+    int k = num.length();
+
+    for(int i=1;i<k-1;i++) {
+        if((num[i-1]-'0'>num[i]-'0' && num[i+1]-'0'>num[i]-'0') ||
+            (num[i-1]-'0'<num[i]-'0' && num[i+1]-'0'<num[i]-'0')) {
+                waviness++;
+            }
     }
-
     return waviness;
-}
-
+} 
 
 
 class Solution {
 public:
     int totalWaviness(int num1, int num2) {
-        
-        int totalWaviness = 0;
+         int totalWaviness = 0;
 
         for(int i=num1;i<=num2;i++) {
-            totalWaviness += calculateWaviness(i);
+            int currWaviness = calculateWaviness(i);
+            totalWaviness += currWaviness;
         }
 
     return totalWaviness;
